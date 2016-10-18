ed067349fdf5edeb4636a9d68a1fca4e58852a76
AlcinaW <white_kaitou@hotmail.com>
2016-10-17 23:54:32 -0400

    Pig latin start
    Comments

---
+++ intermediateAlgorithmScripting/6_pig_latin.js
@@ -0,0 +1,15 @@
+//Translate the provided string to pig latin.
+
+function translatePigLatin(str) {
+  //if str[0] === vowel, return string + "ay"
+  //else take first letter, hold variable
+  //slice str(1) + variable + "ay"
+  
+  //split word into array
+  var letters = str.split('');
+
+  if (str[0] === "a" || "")
+  return str;
+}
+
+translatePigLatin("consonant");
