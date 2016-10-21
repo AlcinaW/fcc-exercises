f16a883e2edc68405799fb1f7b31a5c0ed7fca0e
AlcinaW <white_kaitou@hotmail.com>
2016-10-20 22:00:02 -0400

    Solution and comments

--- intermediateAlgorithmScripting/7_dna_pairings.js
+++ intermediateAlgorithmScripting/7_dna_pairings.js
@@ -1,21 +1,14 @@
 //The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
 
 function pairElement(str) {
-  //split the string into array
-  //make pair arrays to check against? if letter exists in array checked against push other letter
-  //but how to now which one is the one that is not already there?
-  //switch statement, but will have to write for all scenarios
-  //push all new arrays to array for 2d array
-  //for each in the array check
-  var letterArr = str.split(''),
-      pairingArr = [];
-
-      // letterArr.map(function(letter)) {
-      //   pairingArr.push(letter.toUpperCase());
-      // });
-
-  // should return [["G", "C"], ["C","G"],["G", "C"]]
-  return pairingArr;
+  //each key has a value
+  var letterPairs = { A: 'T', T: 'A', C: 'G', G: 'C'},
+    //letter array with string split up (can technically skip this)
+      letterArr = str.split('');
+    //use map to read through letterArr, return character in original string + corresponding value
+    return letterArr.map(function(letter) {
+      return [letter, letterPairs[letter]];
+    });
 }
 
 pairElement("GCG");
