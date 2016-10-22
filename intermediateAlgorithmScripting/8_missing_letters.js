c4ad6128329ea967360b379e3048d9003db8405f
AlcinaW <white_kaitou@hotmail.com>
2016-10-21 21:13:01 -0400

    Start and comments

---
+++ intermediateAlgorithmScripting/8_missing_letters.js
@@ -0,0 +1,18 @@
+// Find the missing letter in the passed letter range and return it.
+// If all letters are present in the range, return undefined.
+
+//String.prototype.charCodeAt()
+//String.fromCharCode()
+
+//Use 2 arrays?
+//String.fromCharCode(65, 66, 67); is "ABC"; in this case, count up from 65?
+//if the string always starts from a smaller number -> take smallest/first letter and the largest/last letter
+//check length of string is same as difference(?), return undefined
+//if there is a difference, match the numbers to the letter, the number that is missing, return the letter
+
+
+function fearNotLetter(str) {
+  return str;
+}
+
+fearNotLetter("abce");
