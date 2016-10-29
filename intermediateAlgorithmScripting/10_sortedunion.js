81b03ddf8288feccad868652ca57816ab50926aa
AlcinaW <white_kaitou@hotmail.com>
2016-10-29 01:00:38 -0400

    Solution con't

--- intermediateAlgorithmScripting/10_sortedunion.js
+++ intermediateAlgorithmScripting/10_sortedunion.js
@@ -1,15 +1,22 @@
 // Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
 
 // In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
-
+//need to loop through entire array (in case has 2+ arrays in it)
+//loop through the arrays inside
+//check and hold the unique values, push to new array
+//go back and recheck new array against current list to ensure no duplicates?
 
 function uniteUnique(arr) {
   //new array for unique values
   var uniqueArr = [];
-  //need to loop through entire array (in case has 2+ arrays in it)
-  //loop through the arrays inside
-  //check and hold the unique values, push to new array
-  //go back and recheck new array against current list to ensure no duplicates?
+  //maybe make the place in array a named var, length
+  for (var i = 0; i < arguments.length; i++) {
+     for (var j = 0; j < arguments[i].length; j++) {
+       if () {
+         //check
+       }
+     }
+   }
   return arr;
 }
 
