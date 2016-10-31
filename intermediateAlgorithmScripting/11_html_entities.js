dd53291153e60ee0412bf6cf9d0ade21ec129eb2
AlcinaW <white_kaitou@hotmail.com>
2016-10-31 00:08:34 -0400

    Regex start

---
+++ intermediateAlgorithmScripting/11_html_entities.js
@@ -0,0 +1,10 @@
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+//use Regex and replace, whoooo
+
+function convertHTML(str) {
+  // &colon;&rpar;
+  return str;
+}
+
+convertHTML("Dolce & Gabbana");
