eada0c6e7fd07c0c826d733e1167550006ad033c
AlcinaW <white_kaitou@hotmail.com>
2016-11-01 23:26:24 -0400

    Solution

--- intermediateAlgorithmScripting/11_html_entities.js
+++ intermediateAlgorithmScripting/11_html_entities.js
@@ -1,9 +1,9 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 
-//use Regex and replace, whoooo
-
 function convertHTML(str) {
-  // &colon;&rpar;
+  //use Regex to match the entity, replace with the second part
+  str = str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g,"&quot;").replace(/'/g,"&apos;");
+
   return str;
 }
 
