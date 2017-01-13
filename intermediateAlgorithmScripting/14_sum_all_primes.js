fff97768f78aedfcc6907e56e26fd21113624031
Alcina <white_kaitou@hotmail.com>
2017-01-12 23:33:42 -0500

    Comments

--- intermediateAlgorithmScripting/14_sum_all_primes.js
+++ intermediateAlgorithmScripting/14_sum_all_primes.js
@@ -2,6 +2,11 @@
 
 // A prime number is defined as a number greater than one and having only two divisors, one and itself. For example, 2 is a prime number because it's only divisible by one and two.
 
+//create array with numbers between 1 and the final number (what if it is a large number?)
+//how to check if prime?
+//what if create array of already existing time and compare?
+//or check each by dividing by numbers in from of it -> if divisible, move to next number because not prime
+
 function sumPrimes(num) {
   return num;
 }
