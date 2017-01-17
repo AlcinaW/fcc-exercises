0223871f4bda8086939729447826b468966784d4
Alcina <white_kaitou@hotmail.com>
2017-01-17 02:53:20 -0500

    solution

--- intermediateAlgorithmScripting/14_sum_all_primes.js
+++ intermediateAlgorithmScripting/14_sum_all_primes.js
@@ -4,25 +4,34 @@
 
 //create array with numbers between 1 and the final number (what if it is a large number?)
 //how to check if prime?
-//what if create array of already existing time and compare?
+//what if create array of already existing prime numbers and compare?
 //or check each by dividing by numbers in from of it -> if divisible, move to next number because not prime
 
 //SIEVE -> divide by certain key numbers -> divide by 2? remove all possibilities
 //https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
+//start from 2 because all numbers can divide by 1
+
 function sumPrimes(num) {
-  var primeArr=[]; //array where primes will be pushed after checked
-  for (var i=2; i <= num; i++){
-    for (var j=2; j <=i; j++) {
-      if (i===j) {
-        primeArr.push(i);
-      }
-      if (i % j === 0) {
-        break;
-      }
+  var primeArr=[];
+
+  for (var i = 2; i <= num; i++){
+    if(isPrimeNum(i)) {
+      primeArr.push(i);
     }
   }
-  return num;
+  return primeArr.reduce(function(a,b) {
+    return a + b;
+  });
+
+  function isPrimeNum(num) {
+    for (var x = 2; x < num; x++) {
+      if (num % x === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
 
 sumPrimes(10);
