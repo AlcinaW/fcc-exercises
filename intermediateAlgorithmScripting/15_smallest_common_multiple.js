d8b8f2689d30d347abd20cfcc898fdc6fe4a6d10
Alcina <white_kaitou@hotmail.com>
2017-01-18 23:32:11 -0500

    minimal

--- intermediateAlgorithmScripting/15_smallest_common_multiple.js
+++ intermediateAlgorithmScripting/15_smallest_common_multiple.js
@@ -5,9 +5,15 @@
 // The range will be an array of two numbers that will not necessarily be in numerical order.
 //https://www.mathsisfun.com/least-common-multiple.html
 //compare arrays? but would not be efficient to check each spot
-//figure out which is the smaller number to loop up 
+//figure out which is the smaller number to loop up
 
 function smallestCommons(arr) {
+  //find min and max numbers, push to array in order of smaller to larger number
+  var min = Math.min(arr[0], arr[1]);
+  var max = Math.max(arr[0], arr[1]);
+
+
+
   return arr;
 }
 
