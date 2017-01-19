91e06a11d6ba8ba6c95149279576a2bcdd567993
Alcina <white_kaitou@hotmail.com>
2017-01-18 22:52:37 -0500

    lowest multiple comments

--- intermediateAlgorithmScripting/15_smallest_common_multiple.js
+++ intermediateAlgorithmScripting/15_smallest_common_multiple.js
@@ -3,6 +3,8 @@
 // Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
 
 // The range will be an array of two numbers that will not necessarily be in numerical order.
+//https://www.mathsisfun.com/least-common-multiple.html
+//compare arrays? but would not be efficient to check each spot 
 
 function smallestCommons(arr) {
   return arr;
