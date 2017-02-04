658236214ed5a6692ae2f6ad62ab39bc2dea0b53
Alcina <white_kaitou@hotmail.com>
2017-02-04 01:17:56 -0500

    solution

--- intermediateAlgorithmScripting/18_steamroller.js
+++ intermediateAlgorithmScripting/18_steamroller.js
@@ -4,9 +4,12 @@
 
 function steamrollArray(arr) {
   // I'm a steamroller, baby
-
-  return Array.isArray(1);
-
+  return arr.reduce(function(acc, next) {
+    //concat can have array as argument, if value is an array
+    //if true, is array, keep going (recursion?)
+    //if false, go to after semi-colon
+    return acc.concat(Array.isArray(next) ? steamrollArray(next) : next);
+  }, []);
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
