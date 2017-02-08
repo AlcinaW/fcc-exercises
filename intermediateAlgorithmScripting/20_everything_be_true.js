4c589461c3007b7c00e939451ff669140ba925ab
Alcina <white_kaitou@hotmail.com>
2017-02-08 00:33:06 -0500

    everything be true start

---
+++ intermediateAlgorithmScripting/20_everything_be_true.js
@@ -0,0 +1,9 @@
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return pre;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
