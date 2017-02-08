52d36eacd2c840fa36f671131e2c028a1b023365
Alcina <white_kaitou@hotmail.com>
2017-02-07 22:37:17 -0500

    binary agent solution

--- intermediateAlgorithmScripting/19_binary_agents.js
+++ intermediateAlgorithmScripting/19_binary_agents.js
@@ -2,11 +2,12 @@
 // The binary string will be space separated.
 // String.prototype.charCodeAt()
 // String.fromCharCode()
-
+//binary can be read in JS?
 
 function binaryAgent(str) {
-  //binary can be read in JS?
-  return str;
+  return str.split(' ').map(function(val){
+    return String.fromCharCode(parseInt(val, 2));
+  }).join('');
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
