7bbd4a00dca6c26066d1ab73219fbce1df0ced3a
Alcina <white_kaitou@hotmail.com>
2017-02-25 20:41:20 -0500

    solution start

--- intermediateAlgorithmScripting/20_everything_be_true.js
+++ intermediateAlgorithmScripting/20_everything_be_true.js
@@ -3,7 +3,29 @@
 
 function truthCheck(collection, pre) {
   // Is everyone being true?
-  return pre;
+  //If pre exists in all == true, if not, false
+  //switch?
+  //WHAT IF THE PROPERTY EXISTS BUT THE VALUE IS EMPTY!?!!?!?! or undefined
+  //collection.hasOwnProperty();
+//   for(var key in collection) {
+//     if(collection.hasOwnProperty(key)) {
+//         collection[key];
+//     }
+// }
+
+
+  // var pre = pre.toString();
+  //
+  // for (var i = 0; i < collection.length; i++) {
+  //   if (!(pre in collection[i])) {
+  //       console.log("NOOOO");
+  //       return false;
+  //     }
+  //     else {
+  //       return true;
+  //     }
+  //     return pre;
+  //   }
 }
 
-truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
