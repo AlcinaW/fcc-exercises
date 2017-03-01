f905529273a0096a11bca2216dab6b1a9278f3c9
Alcina <white_kaitou@hotmail.com>
2017-02-28 21:57:08 -0500

    solution

--- intermediateAlgorithmScripting/20_everything_be_true.js
+++ intermediateAlgorithmScripting/20_everything_be_true.js
@@ -1,33 +1,13 @@
 // Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
 
-
 function truthCheck(collection, pre) {
   // Is everyone being true?
-  //If pre exists in all == true, if not, false
-  //switch statement? length check?
-  //check length? but what if the property exists by the value is empty or undefined
-  //set value to true unless something changes it
   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
   //collection.hasOwnProperty();
-//   for(var key in collection) {
-//     if(collection.hasOwnProperty(key)) {
-//         collection[key];
-//     }
-// }
-
-
-  // var pre = pre.toString();
-  //
-  // for (var i = 0; i < collection.length; i++) {
-  //   if (!(pre in collection[i])) {
-  //       console.log("NOOOO");
-  //       return false;
-  //     }
-  //     else {
-  //       return true;
-  //     }
-  //     return pre;
-  //   }
-}
+  //use bracket notation to access the pre
+  return collection.every(function(obj){
+      return obj[pre];
+    });
+  }
 
 console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
