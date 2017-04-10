2a1a516661de782627dfa135b3c1a1173d65a253
Alcina <white_kaitou@hotmail.com>
2017-04-09 23:12:24 -0400

    telephone number start

---
+++ advancedAlgorithmScripting/1_telephone_no.js
@@ -0,0 +1,6 @@
+//Validate US Telephone Numbers
+//Return true if the passed string is a valid US phone number.
+//The user may fill out the form field any way they choose as long as it is a valid US number.
+//RegExp
+
+console.log("IT'S ALIVEEEEE");
