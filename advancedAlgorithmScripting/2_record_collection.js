3ce8657ba9bc3ee79796e0cfc07fa4090d679b25
Alcina <white_kaitou@hotmail.com>
2017-04-15 00:52:43 -0400

    Record collection solution

--- advancedAlgorithmScripting/2_record_collection.js
+++ advancedAlgorithmScripting/2_record_collection.js
@@ -1,3 +1,47 @@
 // You are given a JSON object representing a part of your musical album collection. Each album has several properties and a unique id number as its key. Not all albums have complete information.
 
 // Write a function which takes an album's id (like 2548), a property prop (like "artist" or "tracks"), and a value (like "Addicted to Love") to modify the data in this collection.
+
+// Setup
+var collection = {
+    2548: {
+      album: "Slippery When Wet",
+      artist: "Bon Jovi",
+      tracks: [
+        "Let It Rock",
+        "You Give Love a Bad Name"
+      ]
+    },
+    2468: {
+      album: "1999",
+      artist: "Prince",
+      tracks: [
+        "1999",
+        "Little Red Corvette"
+      ]
+    },
+    1245: {
+      artist: "Robert Palmer",
+      tracks: [ ]
+    },
+    5439: {
+      album: "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
+function update(id, prop, value) {
+    if (prop === "tracks" && value !== "") {
+      collection[id][prop].push(value);
+    } else if (value !== ""){
+      collection[id][prop] = value;
+    } else {
+      delete collection[id][prop];
+    }
+  return collection;
+}
+
+// Alter values below to test your code
+update(5439, "artist", "ABBA");
