f0183136a14bb518a0e94a16b219880f643ccbf6
Alcina <white_kaitou@hotmail.com>
2017-04-14 22:13:40 -0400

    telephone regex solution

--- advancedAlgorithmScripting/1_telephone_no.js
+++ advancedAlgorithmScripting/1_telephone_no.js
@@ -3,4 +3,12 @@
 //The user may fill out the form field any way they choose as long as it is a valid US number.
 //RegExp
 
-console.log("IT'S ALIVEEEEE");
+
+function telephoneCheck(str) {
+  // Good luck!
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  return regex.test(str);
+}
+
+
+console.log(telephoneCheck("555-555-5555"));
