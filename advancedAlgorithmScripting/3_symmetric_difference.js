4bfd041d7ca8595870633af3905a91ec2d887984
Alcina <white_kaitou@hotmail.com>
2017-04-20 23:38:58 -0400

    notes

--- advancedAlgorithmScripting/3_symmetric_difference.js
+++ advancedAlgorithmScripting/3_symmetric_difference.js
@@ -1,6 +1,7 @@
 // Create a function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays.
 // Array.prototype.reduce()
 //https://www.youtube.com/watch?v=PxffSUQRkG4
+//https://en.wikipedia.org/wiki/Symmetric_difference
 
 function sym(args) {
   return args;
