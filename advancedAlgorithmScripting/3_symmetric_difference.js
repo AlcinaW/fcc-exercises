9b640fc10e6fb9030acd0aae8de79467692d4d18
Alcina <white_kaitou@hotmail.com>
2017-04-21 22:19:07 -0400

    notes

--- advancedAlgorithmScripting/3_symmetric_difference.js
+++ advancedAlgorithmScripting/3_symmetric_difference.js
@@ -2,6 +2,9 @@
 // Array.prototype.reduce()
 //https://www.youtube.com/watch?v=PxffSUQRkG4
 //https://en.wikipedia.org/wiki/Symmetric_difference
+//the numbers that appear only once out of all the arrays
+//must loop though all arrays...check first set against second, make new array, check with next array? but what if the items are common to more than one array?
+//must compare all...efficient way to check?
 
 function sym(args) {
   return args;
