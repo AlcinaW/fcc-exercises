e02af7a6b515a791ce55be492b662ecb549975ec
Alcina <white_kaitou@hotmail.com>
2017-04-29 00:52:01 -0400

    symmetric different solution

--- advancedAlgorithmScripting/3_symmetric_difference.js
+++ advancedAlgorithmScripting/3_symmetric_difference.js
@@ -5,9 +5,36 @@
 //the numbers that appear only once out of all the arrays
 //must loop though all arrays...check first set against second, make new array, check with next array? but what if the items are common to more than one array?
 //must compare all...efficient way to check?
+//can use for to count up
 
+//use hoisting to seperate
+function diffArray (arr1, arr2) {
+  var newArr = arr1.concat(arr2);
+  return newArr.filter(function(value, index, array){
+    if (array.slice(index+1).indexOf(value) === -1 && array.slice(0, index).indexOf(value) === -1) {
+    return value;
+    }
+  });
+}
+function removeDuplicates(arr) {
+  var result = [];
+  arr.forEach(function(value){
+    if (result.indexOf(value) < 0) {
+      result.push(value);
+    }
+  });
+  return result;
+}
 function sym(args) {
-  return args;
+  args = Array.prototype.slice.call(arguments);
+
+  return args.reduce(function(acc, next){
+    //accumulater
+    acc = removeDuplicates(acc);
+    next = removeDuplicates(next);
+    acc = diffArray(acc, next);
+    return acc;
+  }, []);
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
